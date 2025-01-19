bae45e584679eff1dc2a4d0f6abce1a3a43a177c
Victor-Gurbani <89997914+victor-gurbani@users.noreply.github.com>
2025-01-19 23:45:11 +0100

    Created start screen

--- views/boilerplate_startscreen.c
+++ views/boilerplate_startscreen.c
@@ -30,10 +30,10 @@ void boilerplate_startscreen_draw(Canvas* canvas, BoilerplateStartscreenModel* m
     canvas_clear(canvas);
     canvas_set_color(canvas, ColorBlack);
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str_aligned(canvas, 64, 10, AlignCenter, AlignTop, "Start Screen");
+    canvas_draw_str_aligned(canvas, 64, 10, AlignCenter, AlignTop, "NFC HID Reader");
     canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str_aligned(canvas, 64, 22, AlignCenter, AlignTop, "Explain your app");
-    canvas_draw_str_aligned(canvas, 64, 32, AlignCenter, AlignTop, "on this screen");
+    canvas_draw_str_aligned(canvas, 64, 22, AlignCenter, AlignTop, "Connect to a computer");
+    canvas_draw_str_aligned(canvas, 64, 32, AlignCenter, AlignTop, "and start reading NFC tags");
     snprintf(buffer, sizeof(buffer), "Version: %s", BOILERPLATE_VERSION);
     canvas_draw_str_aligned(canvas, 64, 42, AlignCenter, AlignTop, buffer);
     elements_button_center(canvas, "Start");
